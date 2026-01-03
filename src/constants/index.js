470ffa2c34042836036a18c83a956c641b4e71b8
hin22001 <hin22001@gmail.com>
2026-01-03 23:43:17 +0800

    :pencil2: fix capital letters

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
-export { default as EXPERIENCES } from './EXPERIENCES.js';
-export { default as PROJECTS } from './PROJECTS.js';
-export { default as SKILLS } from './SKILLS.js';
+export { default as EXPERIENCES } from './experiences.js';
+export { default as PROJECTS } from './projects.js';
+export { default as SKILLS } from './skills.js';
